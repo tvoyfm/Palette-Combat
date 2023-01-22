d3c41e50810c91863bb0a07203ac93e4ea93f29d
Gleb Stolyarchuk <stolyarchuk.gleb@gmail.com>
2023-01-22 18:32:30 +0100

    Initial Commit

---
+++ PaletteCombat/PaletteCombatApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaletteCombatApp.swift
+//  PaletteCombat
+//
+//  Created by Глеб Столярчук on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct PaletteCombatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
