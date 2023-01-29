86175d4ffa0b0d7dd0dc09454f42c7d406855d13
Gleb Stolyarchuk <stolyarchuk.gleb@gmail.com>
2023-01-29 15:28:58 +0100

    Change UI for color information view

--- PaletteCombat/View/CompareTwoColorView.swift
+++ PaletteCombat/View/CompareTwoColorView.swift
@@ -8,21 +8,32 @@
 import SwiftUI
 
 struct CompareTwoColorView: View {
-
-    @StateObject var leftColor = ComparableColor(.purple)
-    @StateObject var rightColor = ComparableColor(.pink)
-        
+    @StateObject var leftColor = ComparableColor(.cyan)
+    @StateObject var rightColor = ComparableColor(.blue)
+    
     var body: some View {
-        ZStack(alignment: .bottom) {
-            backgroundBlurView()
-            VStack(alignment: .leading, spacing: 10) {
-                colorCompareView()
-                colorGradientView()
-                colorInfoView()
-                colorChangeView()
+        VStack(spacing: 0) {
+            ZStack(alignment: .bottom) {
+                backgroundBlurView()
+                VStack(alignment: .leading, spacing: 10) {
+                    colorInfoView()
+                    colorCompareView()
+                    colorGradientView()
+                }
+                .padding()
             }
-            .padding()
+            barView()
         }
+    }
+    
+    var distanceTintColor: Color {
+        let compareInfo = ColorCompareInfo(leftColor.color, rightColor.color)
+        let max = compareInfo.euclideanRangeMax
+        let current = compareInfo.euclideanDistance
+
+        let percent = 1 / (max / current)
+        let hue = 0.34 - percent / 3
+        return Color(hue: hue, saturation: 1, brightness: 1)
     }
     
     // MARK: - View builders
@@ -30,9 +41,7 @@ struct CompareTwoColorView: View {
     func backgroundBlurView() -> some View {
         ZStack {
             Rectangle()
-                .fill(distanceTintColor.opacity(0.15))
-            Rectangle()
-               .fill(Material.regularMaterial)
+                .fill(.white.opacity(0.1))
         }
         .ignoresSafeArea()
     }
@@ -76,6 +85,7 @@ struct CompareTwoColorView: View {
     func colorInfoView() -> some View {
         VStack(spacing: 10) {
             Text("Есть такой же цвет, но с именем?")
+                .font(.subheadline)
             HStack {
                 colorInformation(leftColor, .leading)
                 Spacer()
@@ -93,31 +103,51 @@ struct CompareTwoColorView: View {
         let closestColor: Color = Color(uiColor: UIColor(hex: info?.name.closestNamedHex ?? "") ?? .systemPink)
         
         VStack(alignment: alignment, spacing: 10) {
+            // UP
             VStack(alignment: alignment, spacing: 4) {
                 Text(hasName ? "да, это": "нет, но ближайший")
                     .font(.caption)
                 Text(info?.name.value ?? "[...]")
                     .font(.subheadline)
-                Text(info?.name.closestNamedHex ?? "[#...]")
-                    .font(.subheadline)
-
             }
             .redacted(reason: hasPlaceholder ? .placeholder : .privacy)
 
             VStack(spacing: 0) {
-                Rectangle()
-                    .fill(hasPlaceholder ? .gray.opacity(0.3) : closestColor)
-                    .frame(maxHeight: .infinity)
-                Rectangle()
-                    .fill(color.color)
-                    .frame(maxHeight: .infinity)
+                ZStack(alignment: .top) {
+                    Rectangle()
+                        .fill(hasPlaceholder ? .gray.opacity(0.3) : closestColor)
+                        .frame(maxHeight: .infinity)
+                    
+                    Text(info?.name.closestNamedHex ?? "#000000")
+                        .padding(4)
+                        .frame(maxWidth: .infinity)
+                        .background {
+                            Rectangle()
+                                .fill(.regularMaterial)
+                        }
+                        .redacted(reason: hasPlaceholder ? .placeholder : .privacy)
+                }
+                
+                // DOWN
+                ZStack(alignment: .bottom) {
+                    Rectangle()
+                        .fill(color.color)
+                        .frame(maxHeight: .infinity)
+                    
+                    Text("#\(color.color.hexString)")
+                        .padding(4)
+                        .frame(maxWidth: .infinity)
+                        .background {
+                            Rectangle()
+                                .fill(.regularMaterial)
+                        }
+                }
             }
+            .fontWidth(.expanded)
+            .font(.subheadline)
             .frame(maxHeight: 200)
             .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
             
-            Text("#\(color.color.hexString)")
-                .font(.title2)
-                .fontWidth(.expanded)
         }
         .fontWidth(.expanded)
     }
@@ -129,16 +159,47 @@ struct CompareTwoColorView: View {
             ColorPicker("", selection: $rightColor.color)
                 .rotation3DEffect(.degrees(180), axis: (x: 0, y: 0, z: 1))
         }
+        .frame(maxWidth: 100)
     }
     
-    var distanceTintColor: Color {
-        let compareInfo = ColorCompareInfo(leftColor.color, rightColor.color)
-        let max = compareInfo.euclideanRangeMax
-        let current = compareInfo.euclideanDistance
-
-        let percent = 1 / (max / current)
-        let hue = 0.34 - percent / 3
-        return Color(hue: hue, saturation: 1, brightness: 1)
+    @ViewBuilder
+    func barView() -> some View {
+        HStack(spacing: 30) {
+//            // Info
+//            Button {
+//                print("action")
+//            } label: {
+//                Image(systemName: "questionmark.circle.fill")
+//                    .tint(.black)
+//                    .padding(8)
+//                    .background {
+//                        RoundedRectangle(cornerRadius: 4, style: .continuous)
+//                            .fill(distanceTintColor)
+//                    }
+//            }
+            HStack {
+                ColorPicker("", selection: $leftColor.color)
+                ColorPicker("", selection: $rightColor.color)
+                    .rotation3DEffect(.degrees(180), axis: (x: 0, y: 0, z: 1))
+            }
+            .frame(maxWidth: 100)
+//
+//            // Share
+//            Button {
+//                print("action")
+//            } label: {
+//                Image(systemName: "square.and.arrow.up.fill")
+//                    .tint(.black)
+//                    .padding(8)
+//                    .background {
+//                        RoundedRectangle(cornerRadius: 4, style: .continuous)
+//                            .fill(distanceTintColor)
+//                    }
+//            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(.thickMaterial)
     }
 }
 
